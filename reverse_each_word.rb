842c2f879eaf495d7d40ddc8118148739991048e
Rashard Pleasant <rashard.pleasant@gmail.com>
2019-06-20 00:14:39 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def "reverse_each_word" ("words")
+  words .each do |"word"|
+    puts "Hello there, and how are you?"
+    
+  end
+
+end
+
+def "reverse_each_word" ("words")
+  words.collect.do |"word"|
+    "Hello there, and how are you?"
+  
+    end
+    
+  end 
